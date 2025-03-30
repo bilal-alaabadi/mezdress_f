56ff2eba2ac6e65d60c521b928c66aa57e287766
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-03-30 19:53:59 +0400

    mf

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,6 @@
-export const getBaseUrl = () => {
-    return 'https://genuine-backend.vercel.app';//رابط الباك اند بدل 5000
-}
 // export const getBaseUrl = () => {
-//     return 'http://localhost:5000';//رابط الباك اند بدل 5000
+//     return 'https://genuine-backend.vercel.app';//رابط الباك اند بدل 5000
 // }
+export const getBaseUrl = () => {
+    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+}
