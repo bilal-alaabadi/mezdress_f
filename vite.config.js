e68419fb6f2684edf80414b59f66862c0b464324
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-04-23 22:09:35 +0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//     plugins: [react()],
-//     server: {
-//         proxy: {
-//             '/api': {
-//                 target: 'https://genuine-backend.vercel.app', // رابط الخادم الخلفي
-//                 changeOrigin: true, // تغيير الأصل للخادم الخلفي
-//                 rewrite: (path) => path.replace(/^\/api/, ''), // إزالة /api من الرابط
-//             },
-//         },
-//     },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://genuine-b.vercel.app', // رابط الخادم الخلفي
+                changeOrigin: true, // تغيير الأصل للخادم الخلفي
+                rewrite: (path) => path.replace(/^\/api/, ''), // إزالة /api من الرابط
+            },
+        },
+    },
+});
