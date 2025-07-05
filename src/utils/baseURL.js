62ca24e3bdcc636b51d6c049983ae899e4fd4365
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-07-05 20:43:03 +0400

    وصف التغييرات هنا

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 // export const getBaseUrl = () => {
-//     return 'https://ightt-b.vercel.app';//رابط الباك اند بدل 5000
+//     return 'http://localhost:5003';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
     return 'https://www.lightoman.shop';//رابط الباك اند بدل 5000 رجع
